ccc9a7ab635d4d91459b0f4f2868d3f317195ea9
Alex Wells <thealee@gmail.com>
2013-05-02 23:23:32 +0100

    deleted index

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ $( "#sortable" ).disableSelection();
 	<div class="clear"></div>
 </div>
 <div>
-<iframe width="100%" height="100%" frameborder="0" src="signs.application/index.html"></iframe>
+<iframe width="100%" height="100%" frameborder="0" src="apps/signs.application/index.html"></iframe>
 </div>
 </body>
 </html>
