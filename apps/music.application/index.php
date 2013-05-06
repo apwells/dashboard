203aa78808b1eab216a23069e2eee88cade24d1d
Eduard Mucilianu <eduard.mucilianu@yahoo.com>
2013-05-06 17:22:48 +0100

    music interface responsive

--- apps/music.application/index.php
+++ apps/music.application/index.php
@@ -1,15 +1,47 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style.css"></link>
 			<title>Music</title>
-	</head>
-	<body>
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js">
+	</script>
+	<script>
+	
+	$(document).ready(function(){
+	$("#pause").hide();
+	var change = function changeimg(id,normal,over){
+	
+	id.mouseenter(function(){
+	id.attr("src",over);}).mouseleave(function(){id.attr("src",normal);
+		});
+	
+	};
+		
+		
+	change($("#back"),"images/back.png","images/backover.png");
+	change($("#next"),"images/next.png","images/nextover.png");	
+	change($("#play"),"images/play.png","images/playover.png");
+	change($("#pause"),"images/pause.png","images/pauseover.png");		
+	
+	$("#play").click(function(){$("#play").hide();$("#pause").show();});	
+		
+		$("#pause").click(function(){$("#pause").hide();$("#play").show();});	
+		});
+		
+	</script>
+
+</head>
+<body>
+		
 	<div class="controller">
 	<ul>
-	<li id="back"><img src="images/back.png"></li>
-	<li id="pp"><img src="images/play.png"></li>
-	<li id="next"><img src="images/next.png"></li>
+	<li><img id="back" src="images/back.png"></img></li>
+	<li>
+	<img id="play" src="images/play.png"></img>
+	<img id="pause" src="images/pause.png"></img>
+	</li>
+	<li><img id="next" src="images/next.png"></img></li>
 	</ul>
 	</div>
 	<div>
